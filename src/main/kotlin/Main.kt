3f08ed35e4a92737e79ee657dd6528dc8e5f7577
Luca <luca.weimer.42@gmail.com>
2025-08-22 20:33:34 +0200

    improved columns amound changing implementatino

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,39 +2,64 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.*
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 @Composable
 @Preview
 fun App() {
-    val columns = remember { mutableStateListOf(List<Pin?>(4) {null}) }
+    val columns = remember { mutableStateListOf(List<Pin?>(5) {null}) }
 
     Column {
         Board.columns(columns)
 
         Board.placeBalePins()
 
-        TextField(
-            value = columns.size.toString(),
-            onValueChange = { text ->
-                val newSize = text.toIntOrNull() ?: return@TextField
-                val currentSize = columns.size
+        var value by remember { mutableStateOf(columns.size.toString()) }
 
-                if (newSize > currentSize) {
-                    repeat(newSize - currentSize) {
-                        columns.add(List<Pin?>(4) { null })
+        val focusManager = LocalFocusManager.current
+        TextField(
+            value = value,
+            onValueChange = { text ->
+                value = text
+            },
+            modifier = Modifier.onPreviewKeyEvent { event ->
+                if (event.type == KeyEventType.KeyDown) {
+                    if(value.toIntOrNull() == null) return@onPreviewKeyEvent true
+
+                    when (event.key) {
+                        Key.Enter -> {
+                            val newSize = value.toInt()
+                            val currentSize = columns.size
+
+                            if (newSize > currentSize) {
+                                repeat(newSize - currentSize) {
+                                    columns.add(List<Pin?>(5) { null })
+                                }
+                            } else if (newSize in 0..<currentSize) {
+                                repeat(currentSize - newSize) {
+                                    columns.removeLast()
+                                }
+                            }
+                            focusManager.clearFocus()
+                            true
+                        }
+                        Key.Escape -> {
+                            focusManager.clearFocus()
+                            true
+                        }
+                        else -> false
                     }
-                } else if (newSize < currentSize && newSize >= 0) {
-                    repeat(currentSize - newSize) {
-                        columns.removeLast()
-                    }
+                } else {
+                    false
                 }
             },
-            label = { Text("Anzahl der Reihen") }
+            label = { Text("Anzahl der Reihen") },
+            isError = value.toIntOrNull() == null
         )
     }
 }
